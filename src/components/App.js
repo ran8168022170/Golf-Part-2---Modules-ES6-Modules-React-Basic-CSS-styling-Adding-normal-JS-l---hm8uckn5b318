5d347178bd66f2e5a8b4847f1597e20ca0953e24
16bitash <entropy.ash@gmail.com>
2023-01-17 22:34:41 +0530

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,54 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import { useEffect } from "react";
 
 const App = () => {
-  const [renderBall, setRenderBall] = useState(false);
+  const [isBallVisible, setIsBallVisible] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-  });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  const reset = () => {
+    setIsBallVisible(false);
+    setX(0);
+    setY(0);
+  };
+
+  const renderChoice = () => {
+    if (isBallVisible) {
+      return (
+        <div className="ball" style={{ left: `${x}px`, top: `${y}px` }}></div>
+      );
+    }
+
+    return (
+      <button onClick={() => setIsBallVisible(true)} className="start">
+        Start
+      </button>
+    );
+  };
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (event.key === "ArrowRight") {
+        setX(x + 5);
+      }
+      if (event.key === "ArrowUp") {
+        setY(y - 5);
+      }
+      if (event.key === "ArrowLeft") {
+        setX(x - 5);
+      }
+      if (event.key === "ArrowDown") {
+        setY(y + 5);
+      }
+    };
+
+    window.addEventListener("keydown", handler);
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, [x, y]);
 
   return (
     <div className="playground">
@@ -21,5 +59,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
